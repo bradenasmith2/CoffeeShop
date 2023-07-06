c990efbb12f29939256e8590cfc0c47b4f1354cd
bradenasmith2 <bradenasmith@icloud.com>
2023-07-06 14:52:36 -0500

    Order class *complete

--- CoffeeShop/Models/Order.cs
+++ CoffeeShop/Models/Order.cs
@@ -8,6 +8,22 @@ namespace CoffeeShop.Models
 {
     public class Order
     {
-        
+        public List<Item> Items = new List<Item>();
+
+        public void AddItem(Item item)
+        {
+            Items.Add(item);
+        }
+
+        public Double Total()
+        {
+            Double Total = 0;
+            foreach (var item in Items)
+            {
+                Total = Total + item.PriceInDollars();
+            }
+            Math.Round(Total, 2);
+            return Total;
+        }
     }
 }
